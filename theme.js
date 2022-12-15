8e91a4b57550755b44b8ff16fece1ce191a8b729
Soltus <liaoshanyi@qq.com>
2022-12-15 17:57:19 +0800

    no message

--- theme.js
+++ theme.js
@@ -1,9 +1,26 @@
 window.theme = {};
 window.funs = {};
+var fs = null;
+var path = null;
+let isAppMode = document
+  .getElementsByTagName("body")[0]
+  .classList.contains("android")
+  ? false
+  : document.getElementsByTagName("body")[0].classList.contains("body--desktop")
+  ? false
+  : window.siyuan.config.system.os == "windows" ||
+    window.siyuan.config.system.os == "darwin"
+  ? true
+  : false;
+if (isAppMode) {
+  fs = require("fs");
+  path = require("path");
+  console.log("isAppMode");
+}
 
 /**
-* 获取操作系统 'windows' 或 'darwin' (MacOS) 或 'android'
-*/
+ * 获取操作系统 'windows' 或 'darwin' (MacOS) 或 'android'
+ */
 window.theme.OS = window.siyuan.config.system.os;
 
 window.funs.loadStyle = function (href, id = null) {
@@ -17,12 +34,17 @@ window.funs.loadStyle = function (href, id = null) {
 window.funs.updateStyle = function (id, href) {
   let style = document.getElementById(id);
   if (style) {
-      style.setAttribute("href", href);
+    style.setAttribute("href", href);
   } else {
-      window.funs.loadStyle(href, id);
+    window.funs.loadStyle(href, id);
   }
 };
-window.funs.loadScript = function (src, type = "module", async = false, defer = false) {
+window.funs.loadScript = function (
+  src,
+  type = "module",
+  async = false,
+  defer = false
+) {
   const script = document.createElement("script");
   if (type) script.type = type;
   if (async) script.async = true;
@@ -30,7 +52,8 @@ window.funs.loadScript = function (src, type = "module", async = false, defer = 
   script.src = src;
   document.head.appendChild(script);
 };
-window.funs.addURLParam = function (url,
+window.funs.addURLParam = function (
+  url,
   param = {
     v: window.siyuan.config.appearance.themeVer,
   }
@@ -38,14 +61,18 @@ window.funs.addURLParam = function (url,
   let new_url;
   switch (true) {
     case url.startsWith("//"):
-      new_url = new URL(`https:${url}`); break;
+      new_url = new URL(`https:${url}`);
+      break;
     case url.startsWith("http://"):
     case url.startsWith("https://"):
-      new_url = new URL(url); break;
+      new_url = new URL(url);
+      break;
     case url.startsWith("/"):
-      new_url = new URL(url, window.location.origin); break;
+      new_url = new URL(url, window.location.origin);
+      break;
     default:
-      new_url = new URL(url, window.location.origin + window.location.pathname); break;
+      new_url = new URL(url, window.location.origin + window.location.pathname);
+      break;
   }
   for (let [key, value] of Object.entries(param)) {
     new_url.searchParams.set(key, value);
@@ -64,4 +91,74 @@ window.funs.addURLParam = function (url,
       );
   }
 };
-window.funs.loadScript(window.funs.addURLParam("/appearance/themes/Sofill=/script/kernel.js"), undefined, true);
+
+let SelfProtection = localStorage.getItem(
+  "SC_winsay_cp_system__SelfProtection"
+);
+let P = [];
+let bP = "";
+let bP_lili = "";
+path
+  ? (bP = path.join(
+      window.siyuan.config.system.confDir,
+      "appearance",
+      "themes",
+      "Sofill-"
+    ))
+  : null;
+path
+  ? (bP_lili = path.join(
+      window.siyuan.config.system.confDir,
+      "appearance",
+      "themes",
+      "Sofill="
+    ))
+  : null;
+fs
+  ? fs.access(bP, (e) => {
+      if (e) {
+        console.error(e);
+        alert(
+          `主题根目录未正确命名或不存在：【致命错误】${bP} 文件夹不存在。修正错误后，需重启思源并重新选择主题。`
+        );
+      }
+    })
+  : null;
+fs
+  ? fs.access(bP_lili, (e) => {
+      if (e) {
+        console.error(e);
+        alert(
+          `主题根目录未正确命名或不存在：【致命错误】${bP_lili} 文件夹不存在。修正错误后，需重启思源并重新选择主题。`
+        );
+      }
+    })
+  : null;
+if (SelfProtection && SelfProtection === "true") {
+  try {
+    path ? P.push(path.join(bP, "script", "utils", "api.min.js")) : null;
+    path ? P.push(path.join(bP, "script", "CP.js")) : null;
+    path ? P.push(path.join(bP, "script", "config.js")) : null;
+    path ? P.push(path.join(bP, "style", "Init.min.css")) : null;
+    path ? P.push(path.join(bP, "style", "Block.min.css")) : null;
+    path ? P.push(path.join(bP_lili, "script", "kernel.js")) : null;
+    P.forEach((i) => {
+      fs
+        ? fs.readFile(i, "utf-8", (e, data) => {
+            if (e) {
+              console.error(e);
+              alert(`主题自我保护检测到异常：【致命错误】${i} 文件不存在`);
+            }
+          })
+        : null;
+    });
+  } catch (e) {
+    console.error(e);
+  }
+}
+
+window.funs.loadScript(
+  window.funs.addURLParam("/appearance/themes/Sofill=/script/kernel.js"),
+  undefined,
+  true
+);
